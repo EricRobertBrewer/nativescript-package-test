e896fbc9fa70629e699a77fc02cf698039b1e302
Eric Robert Brewer <eric.r.brewer@gmail.com>
2016-12-12 15:39:58 -0800

    Added unique Android ‘package’ and iOS ‘CFBundleIdentifier’. Changed nativescript.id in package.json.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import * as application from "application";
+
+declare var NSBundle: any;
 
 @Component({
     selector: "my-app",
     templateUrl: "app.component.html",
 })
-export class AppComponent {
-    public counter: number = 16;
+export class AppComponent implements OnInit {
 
-    public get message(): string {
-        if (this.counter > 0) {
-            return this.counter + " taps left";
-        } else {
-            return "Hoorraaay! \nYou are ready to start building!";
+    packageId: string = "loading...";
+
+    ngOnInit() {
+        if (application.ios) {
+            this.packageId = NSBundle.mainBundle.bundleIdentifier;
+        } else if (application.android) {
+            this.packageId = application.android.context.getPackageName();
         }
-    }
-    
-    public onTap() {
-        this.counter--;
     }
 }
